0cbee0808fc584cee2300b5db6898acbc6843c67
Dennis Tsang <dennistt@gmail.com>
2011-02-21 13:21:41 -0800

    Initial import of sample code.

---
+++ src/minijava/ir/temp/TempMap.java
@@ -0,0 +1,10 @@
+//This class is obsolote. It is replaced by
+//allowing Temp's themselves to be painted (see the Temp.paint() and
+//Temp.getColor() method.
+
+//package minijava.ir.temp;
+//public interface TempMap {
+//
+//	String tempMap(Temp temp);
+//
+//}
