a4b3a37b70a6a98e2d20e92a1ea27e02eeb7ee1a
Jeff Cheung <jeff.h.cheung@gmail.com>
2011-03-05 16:53:07 -0800

    Updated X86Frame to use IR helper class

--- src/minijava/ir/frame/x86/X86Frame.java
+++ src/minijava/ir/frame/x86/X86Frame.java
@@ -7,9 +7,9 @@ import minijava.ir.interp.Word;
 import minijava.ir.interp.X86SimFrame;
 import minijava.ir.temp.Label;
 import minijava.ir.temp.Temp;
+import minijava.ir.tree.IR;
 import minijava.ir.tree.IRExp;
 import minijava.ir.tree.IRStm;
-import minijava.ir.tree.TEMP;
 import minijava.util.IndentingWriter;
 import minijava.util.List;
 
@@ -20,81 +20,81 @@ public class X86Frame extends Frame {
   public static X86Frame factory = new X86Frame(null, null);
   
   protected int localCount = 0;
-
-	protected X86Frame(Label label, List<Access> formals)
-	{
-		super(label, formals);
-	}
-	
-	public int numLocals()
-	{
-	  return this.localCount;
-	}
-
-	@Override
-	public void dump(IndentingWriter out)
-	{
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public Frame newFrame(Label name, List<Boolean> formalsEscape)
-	{
-	  int offset = FIRST_FORMAL_OFFSET;
-	  
-	  List<Access> newFormals = List.empty();
-	  for(Boolean b : formalsEscape)
-	  {
-	    if(b)
-	    {
-	      newFormals.add(new X86InFrame(offset));
-	      offset += this.wordSize();
-	    }
-	    else
-	    {
-	      newFormals.add(new X86InReg(new Temp()));
-	    }
-	  }
-	  
-	  return new X86Frame(name, newFormals);
-	}
-
-	@Override
-	public Access allocLocal(boolean escapes)
-	{
-		return (escapes) ?  new X86InFrame(-(this.wordSize() * this.localCount++)) :
-		                    new X86InReg(new Temp());
-	}
-
-	@Override
-	public IRExp FP()
-	{
-		return new TEMP(new Temp("ebp"));
-	}
-
-	@Override
-	public IRExp RV()
-	{
-    return new TEMP(new Temp("eax"));
-	}
-
-	@Override
-	public int wordSize()
-	{
-	  // x86 supports 32-bit addressing (4 bytes)
-		return 4;
-	}
-
-	@Override
-	public IRStm procEntryExit1(IRStm body)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public X86SimFrame newSimFrame(Interp interp, List<Word> args)
-	{
-		return new X86SimFrame(interp, factory, args);
-	}
+  
+  protected X86Frame(Label label, List<Access> formals)
+  {
+    super(label, formals);
+  }
+  
+  public int numLocals()
+  {
+    return this.localCount;
+  }
+  
+  @Override
+  public void dump(IndentingWriter out)
+  {
+    // TODO Auto-generated method stub
+  }
+  
+  @Override
+  public Frame newFrame(Label name, List<Boolean> formalsEscape)
+  {
+    int offset = FIRST_FORMAL_OFFSET;
+    
+    List<Access> newFormals = List.empty();
+    for(Boolean b : formalsEscape)
+    {
+      if(b)
+      {
+        newFormals.add(new X86InFrame(offset));
+        offset += this.wordSize();
+      }
+      else
+      {
+        newFormals.add(new X86InReg(new Temp()));
+      }
+    }
+    
+    return new X86Frame(name, newFormals);
+  }
+  
+  @Override
+  public Access allocLocal(boolean escapes)
+  {
+    return (escapes) ?  new X86InFrame(-(this.wordSize() * this.localCount++)) :
+                        new X86InReg(new Temp());
+  }
+  
+  @Override
+  public IRExp FP()
+  {
+    return IR.TEMP(new Temp("ebp"));
+  }
+  
+  @Override
+  public IRExp RV()
+  {
+    return IR.TEMP(new Temp("eax"));
+  }
+  
+  @Override
+  public int wordSize()
+  {
+    // x86 supports 32-bit addressing (4 bytes)
+    return 4;
+  }
+  
+  @Override
+  public IRStm procEntryExit1(IRStm body)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+  @Override
+  public X86SimFrame newSimFrame(Interp interp, List<Word> args)
+  {
+    return new X86SimFrame(interp, factory, args);
+  }
 }
